71fdfdec50e5fa09f7605b9ced27a0887362ac6a
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2023-07-15 14:04:44 +0500

    feat: add 3 buttons

---
+++ lib/const.dart
@@ -0,0 +1,8 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+final mapTypes = [
+  MapType.hybrid,
+  MapType.normal,
+  MapType.satellite,
+  MapType.terrain,
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_example/const.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
@@ -8,7 +12,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,20 +20,32 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final Completer mapCompleter = Completer();
+  LatLng location = const LatLng(41.01, 71.66);
+  int mapTypeIndex = 0;
+  bool showTraffic = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPermission();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,17 +53,80 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Google map'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Stack(
+      body: Stack(
+        alignment: Alignment.topRight,
         children: [
           GoogleMap(
+            onMapCreated: (controller) => mapCompleter.complete(controller),
+            mapType: mapTypes[mapTypeIndex % 4],
             zoomControlsEnabled: false,
-            z
-            compassEnabled: true,
+            trafficEnabled: showTraffic,
+            myLocationEnabled: true,
+            markers: {
+              Marker(markerId: const MarkerId('location'), position: location),
+            },
             initialCameraPosition:
-                CameraPosition(target: LatLng(41.01, 71.66), zoom: 10),
+                const CameraPosition(target: LatLng(41.01, 71.66), zoom: 10),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  child: const Icon(Icons.location_on),
+                  onPressed: () async {
+                    final pos = await Geolocator.getCurrentPosition();
+                    location = LatLng(pos.latitude, pos.longitude);
+                    setState(() {});
+                    final GoogleMapController contr = await mapCompleter.future;
+                    contr.animateCamera(CameraUpdate.newLatLng(location));
+                  },
+                ),
+                const SizedBox(height: 10),
+                FloatingActionButton(
+                  child: const Icon(Icons.location_city),
+                  onPressed: () {
+                    mapTypeIndex++;
+
+                    setState(() {});
+                  },
+                ),
+                const SizedBox(height: 10),
+                FloatingActionButton(
+                  child: const Icon(Icons.traffic),
+                  onPressed: () {
+                    showTraffic = !showTraffic;
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> _checkPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
   }
 }
